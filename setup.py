db1cc4bf276101da98702f5bf8bc7543e381ea26
Yannick Eyholzer <yannholzer@hotmail.com>
2023-10-09 10:54:32 +0200

    First commit

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+
+setup(
+    # Needed to silence warnings (and to be a worthwhile package)
+    name='mypakcage',
+    url='https://github.com/yannholzer/mypackage',
+    author='Yannick Eyholzer',
+    author_email='yannick.eyholzer@unige.ch',
+    # Needed to actually package something
+    packages=[],
+    # Needed for dependencies
+    install_requires=['numpy'],
+    # *strongly* suggested for sharing
+    version='0.1',
+    # The license can be anything you like
+    license='MIT',
+    description='A python package consisting in useful function for my phd',
+    # We will also need a readme eventually (there will be a warning)
+    # long_description=open('README.txt').read(),
+)
