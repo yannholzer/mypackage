88d9b4903a9ac773abdbfd20439cf74fc1fd08e1
Yannick Eyholzer <yannholzer@hotmail.com>
2023-11-09 17:05:08 +0100

    add lightcurve generator to generate lc with snr, t0, period et transit duration randomly selected in a given range

--- mypackage/lightcurve/generate_lightcurve.py
+++ mypackage/lightcurve/generate_lightcurve.py
@@ -258,7 +258,7 @@ class Lightcurve:
     
     def __init__(
         self, 
-        t0:float=0, 
+        t0:float=None, 
         period:float=None, 
         transit_duration:float=None, 
         epoch:float=None, 
@@ -271,7 +271,8 @@ class Lightcurve:
         ):
     
     ### check the default value ###
-        self.t0 = t0
+        if t0 is None:
+            t0 = 0
         if period is None:
             period = np.random.uniform(*PERIOD_RANGE)
         if observation_time is None:
@@ -301,9 +302,9 @@ class Lightcurve:
             timing_variation_params[0] *= transit_duration
             timing_variation_params[1] *= observation_time
             
-        
+        self.t0 = t0
         self.period = period
-        self.observation_time = observation_time
+        self.observation_time = observation_time + t0
         self.cadence = cadence
         self.transit_duration = transit_duration
         self.epoch = epoch
@@ -313,11 +314,11 @@ class Lightcurve:
         timing_variation_amplitude, timing_variation_period, timing_variation_phase = self.timing_variation_params
       
         ### instantiate time and flux arrays ###        
-        self.time = np.arange(self.t0, self.observation_time + self.t0, self.cadence, dtype=float)
+        self.time = np.arange(self.t0, self.observation_time, self.cadence, dtype=float)
         self.flux = np.ones_like(self.time)
         
         ### get the transit time according to the parameters ###
-        self.transits_time = np.arange(self.t0 + self.epoch, self.observation_time + self.t0, self.period, dtype=float)
+        self.transits_time = np.arange(self.t0 + self.epoch, self.observation_time, self.period, dtype=float)
         self.transits_time += timing_variation_amplitude*np.sin(2*np.pi*self.transits_time/timing_variation_period + timing_variation_phase)
 
         ### find the left and right limits of each transits and apply the transit fraction
@@ -333,8 +334,9 @@ class Lightcurve:
             
         ### add the noise ###
         if sigma_noise is None and snr is None:
-            self.sigma_noise = (1- self.transit_depth_fraction)
-            self.snr = np.sqrt(n_transiting_points) # because transit depth = sigma_noise, as snr = sqrt(N) * D / sigma
+            self.snr = np.random.uniform(2, 25)
+            #as snr = sqrt(N) * D / sigma
+            self.sigma_noise = np.sqrt(n_transiting_points) * (1 - self.transit_depth_fraction) / self.snr
             self.flux += np.random.normal(0, self.sigma_noise, self.time.shape)
         elif sigma_noise is False or snr is False:
             # do not add any noise
@@ -358,7 +360,7 @@ class Lightcurve:
             
         
         
-class Lightcurve_npy_generator:
+class Lightcurve_npy_generator_snr_range_seedable:
     def __init__(
         self,
         n_data:int, 
@@ -373,6 +375,7 @@ class Lightcurve_npy_generator:
         period_range:Tuple[float, float]=(5, 10), 
         transit_duration_range:Tuple[float, float]=(0.05, 0.1),
         epoch:bool=None,                                        # if None, randomly selected between 0 and period, if False, set to period / 2
+        t0:float=0,
         observation_time:float=4*365,
         cadence:float=29.4 * MIN_TO_DAYS,
         transit_depth_fraction:float=0.995,
@@ -419,7 +422,7 @@ class Lightcurve_npy_generator:
             for n in range(n_data):
                 period = np.random.uniform(*period_range)
                 transit_duration = np.random.uniform(*transit_duration_range)
-                lc = Lightcurve(0, period, transit_duration, epoch, observation_time, cadence, transit_depth_fraction, sigma, snr, timing_variation_params)
+                lc = Lightcurve(t0, period, transit_duration, epoch, observation_time, cadence, transit_depth_fraction, sigma, snr, timing_variation_params)
                 time, flux = lc.get_time_flux()
                 
             
@@ -464,10 +467,112 @@ class Lightcurve_npy_generator:
                         
                 
 
+class Lightcurve_npy_generator_argument_range:
+    def __init__(
+        self,
+        n_data:int, 
+        path_to_export:str,
+        data_name:str,
+        compress:bool=False,
+        subfolder:bool=False,
+        data_format:str="lightcurve",
+        name_period_range_format:str=True,
+        save_params_txt:bool=True,
+        period_range:Tuple[float, float]=(5, 10), 
+        transit_duration_range:Tuple[float, float]=(0.05, 0.1),
+        epoch:bool=None,                                        # if None, randomly selected between 0 and period, if False, set to period / 2
+        t0_range:Tuple[float, float]=(0, 0),
+        observation_time:float=4*365,
+        cadence:float=29.4 * MIN_TO_DAYS,
+        transit_depth_fraction:float=0.995,
+        snr_range:Tuple[float, float]=(2, 25),
+        timing_variation_params_range:list[float, float, float]=[(1, 5), (0.3, 2), (0, 2*np.pi)]
+        
+        ):
+        
+        data_formats = ["lightcurve", "river_diagram"]
+    
+        if data_format not in data_formats:
+            raise ValueError(f"data format not valid. please select a format from {data_formats}")
+    
+    
+   
+        path_root = os.path.join(path_to_export, data_format, "raw")
+        os.makedirs(path_root, exist_ok=True)
+        path_data = os.path.join(path_root, data_name)
+        try:
+            os.mkdir(path_data)
+        except Exception as e:
+            print(e)
+            try:
+                entry = input("do you want to erase current datas ? (y/N)")
+            except EOFError:
+                print("script is running remotely, delete file manually.")
+                entry = False
+            if entry != "y":
+                raise(SystemExit)
+            shutil.rmtree(path_data)
+            os.mkdir(path_data)
                 
         
-        
     
+        
+        for n in range(n_data):
+            period = np.random.uniform(*period_range)
+            transit_duration = np.random.uniform(*transit_duration_range)
+            snr = np.random.uniform(*snr_range)
+            t0 = np.random.uniform(*t0_range)
+            timing_variation_params = [
+                np.random.uniform(*timing_variation_params_range[0]),
+                np.random.uniform(*timing_variation_params_range[1]),
+                np.random.uniform(*timing_variation_params_range[2]),
+            ]
+            
+            lc = Lightcurve(t0, period, transit_duration, epoch, observation_time, cadence, transit_depth_fraction, None, snr, timing_variation_params)
+            time, flux = lc.get_time_flux()
+            
+        
+            if name_period_range_format:
+                char = "_"
+            else:
+                char = "-"
+            str_snr = f"{lc.snr:.2f}".replace(".", "p")
+            str_p_min = f"{period_range[0]:.2f}".replace(".", "p")
+            str_p_max = f"{period_range[1]:.2f}".replace(".", "p")
+            name = f"lc{str(n).zfill(len(str(n_data-1)))}_{str_snr}_{str_p_min}{char}{str_p_max}"
+                
+                    
+            
+        
+            
+            path_lightcurve = os.path.join(path_data, name)
+                            
+            if data_format is data_formats[0]:
+                if subfolder:
+                    os.mkdir(path_lightcurve)    
+                    path_lightcurve = os.path.join(path_lightcurve, name)
+            if compress:
+                    np.savez(path_lightcurve, time=time, flux=flux)
+            else:
+                data = np.array([time, flux])
+                np.save(path_lightcurve, data)
+            
+            if save_params_txt:
+                d = lc.__dict__
+                d["snr"] = float(d["snr"])
+                d["sigma_noise"] = float(d["sigma_noise"])
+                
+                del d["time"]
+                del d["flux"]
+                del d["flux_pur"]
+                d["transits_time"] = d["transits_time"].tolist()
+                
+                    
+            with open(f"{path_lightcurve}.yml", "w") as f:
+                yaml.safe_dump(d, f)         
+    
+    
+
 
         
 
